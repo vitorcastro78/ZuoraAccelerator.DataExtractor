4a5795fb5016380ace87f79aa88302e75c6ebfcb
Vitor Castro <vitorcastro78@gmail.com>
2025-06-12 19:52:11 +0100

    Refactor AppDataContext and update DbSet properties
    Modified the `AppDataContext` class to include a constructor for `DbContextOptions<AppDataContext>`, enabling dependency injection. Added, removed, and renamed several `DbSet` properties to align with the updated data model, introducing new entities like `Amounts`, `FulfillmentItem`, and `ProductPlan`. Cleaned up code for consistent formatting and naming conventions.

--- Repository/DataContext/DataContext.cs
+++ Repository/DataContext/DataContext.cs
@@ -5,55 +5,69 @@ namespace Repository.DataContext
 {
     public class AppDataContext : DbContext
     {
-        public DbSet<Account> Account{ get; set; }
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Account { get; set; }
 
         public DbSet<Address> Address { get; set; }
 
-        public DbSet<ArTransactions> ArTransaction{ get; set; }
+        public DbSet<Amounts> Amounts { get; set; }
 
-        public DbSet<BillingDocumentItem> BillingDocumentItem{ get; set; }
+        public DbSet<ArTransactions> ArTransaction { get; set; }
 
-        public DbSet<BillingDocument> BillingDocument{ get; set; }
+        public DbSet<BillingDocument> BillingDocument { get; set; }
 
-        public DbSet<BillingDocumentSettings> BillingDocumentSetting{ get; set; }
+        public DbSet<BillingDocumentItem> BillingDocumentItem { get; set; }
 
-        public DbSet<Contact> Contact{ get; set; }
+        public DbSet<BillingDocumentSettings> BillingDocumentSetting { get; set; }
+
+        public DbSet<BillTo> BillTo { get; set; }
+
+        public DbSet<Contact> Contact { get; set; }
+
+        public DbSet<CreditMemo> CreditMemo { get; set; }
 
         public DbSet<CreditMemoAppliedTo> CreditMemoAppliedTo { get; set; }
 
-        public DbSet<CreditMemoItem> CreditMemoItem{ get; set; }
+        public DbSet<CreditMemoItem> CreditMemoItem { get; set; }
 
-        public DbSet<CreditMemo> CreditMemo{ get; set; }
+        public DbSet<CustomObject> CustomObject { get; set; }
 
-        public DbSet<CustomObject> CustomObject{ get; set; }
+        public DbSet<DebitMemo> DebitMemo { get; set; }
 
-        public DbSet<DebitMemoItem> DebitMemoItem{ get; set; }
+        public DbSet<DebitMemoItem> DebitMemoItem { get; set; }
 
-        public DbSet<DebitMemo> DebitMemo{ get; set; }
+        public DbSet<Fulfillment> Fulfillment { get; set; }
 
-        public DbSet<InvoiceItem> InvoiceItem{ get; set; }
+        public DbSet<FulfillmentItem> FulfillmentItem { get; set; }
 
-        public DbSet<Invoice> Invoice{ get; set; }
+        public DbSet<Invoice> Invoice { get; set; }
+
+        public DbSet<InvoiceItem> InvoiceItem { get; set; }
+
+        public DbSet<Money> Money { get; set; }
 
         public DbSet<Order> Order { get; set; }
 
-        public DbSet<OrderItem> OrderLineItem{ get; set; }
+        public DbSet<OrderItem> OrderLineItem { get; set; }
 
-        public DbSet<PaymentMethod> PaymentMethod{ get; set; }
+        public DbSet<Payment> Payment { get; set; }
 
-        public DbSet<Payment> Payment{ get; set; }
+        public DbSet<PaymentMethod> PaymentMethod { get; set; }
 
         public DbSet<PaymentsAppliedTo> PaymentsAppliedTo { get; set; }
 
-        public DbSet<PaymentScheduleItem> PaymentScheduleItem{ get; set; }
+        public DbSet<PaymentScheduleItem> PaymentScheduleItem { get; set; }
 
-        public DbSet<PaymentTransactions> PaymentTransaction{ get; set; }
-
-        public DbSet<Product> Product{ get; set; }
+        public DbSet<PaymentTransactions> PaymentTransaction { get; set; }
 
         public DbSet<Plan> Plan { get; set; }
 
         public DbSet<Price> Price { get; set; }
+
+        public DbSet<Product> Product { get; set; }
 
         public DbSet<ProductPlan> ProductPlan { get; set; }
 
@@ -61,45 +75,44 @@ namespace Repository.DataContext
 
         public DbSet<ProductTier> ProductTier { get; set; }
 
-        public DbSet<Refund> Refund{ get; set; }
+        public DbSet<Refund> Refund { get; set; }
 
-        public DbSet<RefundTransactions> RefundTransaction{ get; set; }
-
-        public DbSet<SubscriptionAddPlan> SubscriptionAddPlan{ get; set; }
-
-        public DbSet<SubscriptionCancel> SubscriptionCancel{ get; set; }
-
-        public DbSet<SubscriptionItem> SubscriptionItem{ get; set; }
-
-        public DbSet<SubscriptionPause> SubscriptionPause{ get; set; }
-
-        public DbSet<SubscriptionPlan> SubscriptionPlan{ get; set; }
-
-        public DbSet<SubscriptionRemovePlan> SubscriptionRemovePlan{ get; set; }
-
-        public DbSet<SubscriptionRenew> SubscriptionRenew{ get; set; }
-
-        public DbSet<Subscription> Subscription{ get; set; }
-
-        public DbSet<SubscriptionTerm> SubscriptionTerm{ get; set; }
-
-        public DbSet<TaxationItem> TaxationItem{ get; set; }
-
-        public DbSet<TaxCertificate> TaxCertificate{ get; set; }
-
-        public DbSet<TaxIdentifier> TaxIdentifier{ get; set; }
-
-        public DbSet<TransactionsState> TransactionsState { get; set; }
-
-        public DbSet<Usage> Usage{ get; set; }
-
-        public DbSet<BillTo> BillTo { get; set; }
+        public DbSet<RefundTransactions> RefundTransaction { get; set; }
 
         public DbSet<SoldTo> SoldTo { get; set; }
 
+        public DbSet<Subscription> Subscription { get; set; }
+
+        public DbSet<SubscriptionAddPlan> SubscriptionAddPlan { get; set; }
+
+        public DbSet<SubscriptionCancel> SubscriptionCancel { get; set; }
+
+        public DbSet<SubscriptionItem> SubscriptionItem { get; set; }
+
+        public DbSet<SubscriptionPause> SubscriptionPause { get; set; }
+
+        public DbSet<SubscriptionPlan> SubscriptionPlan { get; set; }
+
+        public DbSet<SubscriptionRemovePlan> SubscriptionRemovePlan { get; set; }
+
+        public DbSet<SubscriptionRenew> SubscriptionRenew { get; set; }
+
+        public DbSet<SubscriptionTerm> SubscriptionTerm { get; set; }
+
+        public DbSet<TaxationItem> TaxationItem { get; set; }
+
+        public DbSet<TaxCertificate> TaxCertificate { get; set; }
+
+        public DbSet<TaxIdentifier> TaxIdentifier { get; set; }
+
+        public DbSet<Tier> Tier { get; set; }
+
+        public DbSet<TransactionsState> TransactionsState { get; set; }
+
+        public DbSet<Usage> Usage { get; set; }
+
         // Novos DbSets adicionados
         //public DbSet<AddressFieldDefinitions> AddressFieldDefinitions { get; set; }
-        public DbSet<Amounts> Amounts { get; set; }
         //public DbSet<ApplePay> ApplePay { get; set; }
         //public DbSet<ApplePayCreate> ApplePayCreate { get; set; }
         //public DbSet<AchDebit> AchDebit { get; set; }
@@ -110,12 +123,9 @@ namespace Repository.DataContext
         //public DbSet<CustomFields> CustomFields { get; set; }
         //public DbSet<Detail> Detail { get; set; }
         //public DbSet<Error> Error { get; set; }
-        public DbSet<Fulfillment> Fulfillment { get; set; }
-        public DbSet<FulfillmentItem> FulfillmentItem { get; set; }
         //public DbSet<FulfillmentProcessingOption> FulfillmentProcessingOption { get; set; }
         //public DbSet<GetByIdQueryParams> GetByIdQueryParams { get; set; }
         //public DbSet<Headers> Headers { get; set; }
-        public DbSet<Money> Money { get; set; }
         //public DbSet<OrderAction> OrderAction { get; set; }
         //public DbSet<PadDebit> PadDebit { get; set; }
         //public DbSet<PaymentRun> PaymentRun { get; set; }
@@ -149,18 +159,11 @@ namespace Repository.DataContext
         //public DbSet<SubscriptionUpdatePlanPatchResponse> SubscriptionUpdatePlanPatchResponse { get; set; }
         //public DbSet<SubscriptionVersion> SubscriptionVersion { get; set; }
         //public DbSet<SubscriptionVersionOrder> SubscriptionVersionOrder { get; set; }
-        public DbSet<Tier> Tier { get; set; }
+
         //public DbSet<ValidityPeriod> ValidityPeriod { get; set; }
         //public DbSet<WorkflowRun> WorkflowRun { get; set; }
         //public DbSet<WriteOff> WriteOff { get; set; }
         //public DbSet<WriteOffState> WriteOffState { get; set; }
-
-        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
-        {
-        }
-
-       
-
         //protected override void OnModelCreating(ModelBuilder modelBuilder)
         //{
         //    modelBuilder.Entity<Account>().ToTable("Account");
